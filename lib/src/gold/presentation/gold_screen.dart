e8485467768a447439ffaa676fc981972afa2030
Krasimir Varnaliev <gyncoo@abv.bg>
2025-07-01 10:33:36 +0200

    Refactor GoldScreen to use StatefulWidget and implement live gold price updates with StreamBuilder

--- lib/src/gold/presentation/gold_screen.dart
+++ lib/src/gold/presentation/gold_screen.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:gold_stream_app/src/gold/presentation/widgets/gold_header.dart';
 import 'package:intl/intl.dart';
+import 'package:gold_stream_app/src/gold/data/fake_gold_api.dart';
 
-class GoldScreen extends StatelessWidget {
+class GoldScreen extends StatefulWidget {
   const GoldScreen({super.key});
+
+  @override
+  State<GoldScreen> createState() => _GoldScreenState();
+}
+
+class _GoldScreenState extends State<GoldScreen> {
+  final goldPriceStream = getGoldPriceStream();
+
   @override
   Widget build(BuildContext context) {
-    /// Platzhalter für den Goldpreis
-    /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    double goldPrice = 69.22;
-
     return SafeArea(
       child: Scaffold(
         body: Padding(
@@ -24,13 +29,29 @@ class GoldScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
               SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
+              StreamBuilder<double>(
+                stream: goldPriceStream,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (!snapshot.hasData) {
+                    return Text('Kein Kurs verfügbar');
+                  } else {
+                    final goldPrice = snapshot.data!;
+                    return Text(
+                      NumberFormat.simpleCurrency(
+                        locale: 'de_DE',
+                      ).format(goldPrice),
+                      style: Theme.of(
+                        context,
+                      ).textTheme.headlineLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    );
+                  }
+                },
               ),
             ],
           ),
